8db1a38304216c4a156c3d132f11aae6e168a09c
damienbod <damien_bod@hotmail.com>
2025-03-15 13:36:17 +0100

    Update client

---
+++ src/ConsoleClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using ConsoleDcrClient;
+using IdentityModel.Client;
+
+Console.Title = "Client assertion Client";
+
+"\n\nObtaining access token for dynamic client".ConsoleYellow();
+var dynamicClientToken = await RequestTokenAsync("mobile-client", "secret");
+dynamicClientToken.Show();
+Console.ReadLine();
+
+"\n\nCalling API".ConsoleYellow();
+await CallServiceAsync(dynamicClientToken.AccessToken);
+Console.ReadLine();
+
+static async Task<TokenResponse> RequestTokenAsync(string clientId = "mobile-client", string clientSecret = "secret")
+{
+    var client = new HttpClient();
+
+    var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+    if (disco.IsError) throw new Exception(disco.Error);
+
+    var response = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+    {
+        Address = disco.TokenEndpoint,
+
+        ClientId = clientId,
+        ClientSecret = clientSecret,
+    });
+
+    if (response.IsError) throw new Exception(response.Error);
+    return response;
+}
+
+static async Task CallServiceAsync(string token)
+{
+    var baseAddress = Constants.SimpleApi;
+
+    var client = new HttpClient
+    {
+        BaseAddress = new Uri(baseAddress)
+    };
+
+    client.SetBearerToken(token);
+    var response = await client.GetStringAsync("identity");
+
+    "\n\nService claims:".ConsoleGreen();
+    Console.WriteLine(response.PrettyPrintJson());
+}
