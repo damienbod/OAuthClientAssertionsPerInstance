fcb0a31d4da5238b70b37b1615c7472038b22179
damienbod <damien_bod@hotmail.com>
2025-03-17 09:38:11 +0100

    clean up

--- src/PerInstance/IdpPreInstanceAssertion/Controllers/OnboardingController.cs
+++ src/PerInstance/IdpPreInstanceAssertion/Controllers/OnboardingController.cs
@@ -17,7 +17,6 @@ public class OnboardingController : Controller
     /// <summary>
     /// Unsecure API which creates a session
     /// DDOS protection required
-    /// Convert.ToBase64String(rsaCertificate.GetRawCertData())
     /// </summary>
     /// <param name="publicKey">Public key which is used by the session creator</param>
     [HttpPost]
