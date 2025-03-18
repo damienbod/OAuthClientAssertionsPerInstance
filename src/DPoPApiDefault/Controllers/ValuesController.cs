a7552bd0450e414ff608e37c1538a440c5199185
damienbod <damien_bod@hotmail.com>
2025-03-18 20:13:16 +0100

    Update sessionId

--- src/DPoPApiDefault/Controllers/ValuesController.cs
+++ src/DPoPApiDefault/Controllers/ValuesController.cs
@@ -10,6 +10,7 @@ public class ValuesController : Controller
     [HttpGet]
     public IEnumerable<string> Get()
     {
+        var sessionId = GetSessionId();
         // debugging info
         var authHeader = Request.Headers.Authorization;
         var claims = User.Claims.Select(c => new { c.Type, c.Value });
@@ -17,7 +18,14 @@ public class ValuesController : Controller
         return
         [
             "data 1 from the api protected using OAuth DPoP",
-            "data 2 from the api"
+            $"data 2 from the api, sessionId from AT: {sessionId}"
         ];
+    }
+
+    private string? GetSessionId()
+    {
+        var sessionIdClaim = User.Claims.FirstOrDefault(c => c.Type == "scope" && c.Value.StartsWith("sessionId"));
+        var sessionId = sessionIdClaim?.Value.Replace("sessionId:", "");
+        return sessionId;
     }
 }
