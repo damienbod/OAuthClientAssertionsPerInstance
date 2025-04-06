668271598eee5bb5fa24c6c7db13d6c66b30ae0e
damienbod <damien_bod@hotmail.com>
2025-04-06 12:34:33 +0200

    Update docs

--- src/Idp/Controllers/OnboardingController.cs
+++ src/Idp/Controllers/OnboardingController.cs
@@ -17,6 +17,7 @@ public class OnboardingController : Controller
     /// <summary>
     /// Unsecure API which creates a session
     /// DDOS protection required
+    /// Add nonce to prevent reply attacks
     /// </summary>
     /// <param name="publicKey">Public key which is used by the session creator</param>
     [HttpPost]
