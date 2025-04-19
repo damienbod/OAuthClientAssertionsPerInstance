9d24d8e98e73844c40e8a2b955e7a6c8ae83a81f
damienbod <damien_bod@hotmail.com>
2025-04-19 12:04:06 +0200

    Add some DTOs

---
+++ AuthFlow/OAuthConsts.cs
@@ -0,0 +1,34 @@
+﻿namespace AuthFlow;
+
+public class OAuthConsts
+{
+    public const string ERROR_INVALID_REQUEST = "invalid_request";
+    public const string ERROR_INVALID_CLIENT = "invalid_client";
+    public const string ERROR_INVALID_GRANT = "invalid_grant";
+    public const string ERROR_UNAUTHORIZED_CLIENT = "unauthorized_client";
+    public const string ERROR_UNSUPPORTED_GRANT_TYPE = "unsupported_grant_type";
+    public const string ERROR_INVALID_SCOPE = "invalid_scope";
+
+    public const string REQUEST_GRANT_TYPE = "grant_type";
+    public const string REQUEST_PUBLIC_KEY = "public_key";
+    public const string REQUEST_STATE = "state";
+    public const string REQUEST_CLIENT_ID = "client_id";
+    public const string REQUEST_NONCE = "nonce";
+
+    public const string RESPONSE_TOKEN_TYPE = "token_type";
+    public const string RESPONSE_EXPIRES_IN = "expires_in";
+    public const string RESPONSE_FP_TOKEN = "fp_token";
+ 
+    // https://www.rfc-editor.org/rfc/rfc6749#section-5.2
+    public const string RESPONSE_ERROR = "error";
+    public const string RESPONSE_ERROR_DESCRIPTION = "error_description";
+    public const string RESPONSE_ERROR_URI = "error_uri";
+
+    // extra claims in error response but not in spec
+    public const string RESPONSE_ERROR_CODES = "error_codes";
+    public const string RESPONSE_ERROR_TIMESTAMP = "timestamp";
+    public const string RESPONSE_ERROR_TRACE_ID = "trace_id";
+    public const string RESPONSE_ERROR_CORRELATION_ID = "correlation_id";
+    public const string RESPONSE_ERROR_CLAIMS = "claims";
+
+}
--- src/Idp/Controllers/DeviceRegistrationController.cs
+++ src/Idp/Controllers/DeviceRegistrationController.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Services;
+﻿using AuthFlow;
+using Duende.IdentityServer.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -105,48 +106,4 @@ public class DeviceRegistrationController : Controller
 
         return tokenHandler.WriteToken(token);
     }
-}
-
-public class DeviceRegistrationRequest
-{
-    //client_id=cid_235saw4r4
-    //&grant_type=fp_register
-    //&public_key=<public_key>
-    //&state=<state>
-    //&nonce=<nonce>
-
-    [JsonPropertyName("client_id")]
-    public string client_id { get; set; } = string.Empty;
-
-    [JsonPropertyName("grant_type")]
-    public string grant_type { get; set; } = string.Empty;
-
-    [JsonPropertyName("public_key")]
-    public string public_key { get; set; } = string.Empty;
-
-    [JsonPropertyName("state")]
-    public string state { get; set; } = string.Empty;
-
-    [JsonPropertyName("nonce")]
-    public string nonce { get; set; } = string.Empty;
-}
-
-public class DeviceRegistrationResponse
-{
-    //"fp_token": "2YotnFZFEjr1zCsicMWpAA",
-    //"token_type": "fp+jwt",
-    //"state": "<state>"
-    //"expires_in": 420
-
-    [JsonPropertyName("fp_token")]
-    public string FpToken { get; set; } = string.Empty;
-
-    [JsonPropertyName("token_type")]
-    public string TokenType { get; set; } = "fp+jwt";
-
-    [JsonPropertyName("state")]
-    public string State { get; set; } = string.Empty;
-
-    [JsonPropertyName("expires_in")]
-    public string ExpiresIn { get; set; } = "420";
 }
